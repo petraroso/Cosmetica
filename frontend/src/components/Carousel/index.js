d447c369cbdf1912086020cc2ca1a33d5b356e3d
petraroso <proso00@fesb.hr>
2024-05-24 15:09:25 +0200

    Removed spinner

--- frontend/src/components/Carousel/index.js
+++ frontend/src/components/Carousel/index.js
@@ -3,7 +3,6 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Spinner from "../Spinner";
 
 export default function CarouselComponent({ getBestsellers }) {
   const [bestsellers, setBestsellers] = useState([]);
@@ -22,43 +21,37 @@ export default function CarouselComponent({ getBestsellers }) {
   //if bestsellers.length>0 show carousel
   return (
     <div className={styles.carouselContainer}>
-      {bestsellers.length>0 ? (
-        <Carousel
-          autoPlay
-          infiniteLoop
-          useKeyboardArrows
-          dynamicHeight={true}
-          interval={4500}
-          transitionTime={2000}
-          showStatus={false}
-          showThumbs={false}
-        >
-          {bestsellers.map((item, idx) => (
-            <Link
-              key={idx}
-              className={styles.link}
-              to={`/product-details/${item._id}/`}
-            >
-              <div>
-                <img
-                  crossOrigin="anonymous"
-                  src={item.images ? item.images[0].path : null}
-                  className={styles.image}
-                  alt="carousel"
-                />
-                <div className={styles.legend}>
-                  <h3>
-                    Najprodavaniji proizvod iz kategorije {item.category}:
-                  </h3>
-                  <h3>{item.name}</h3>
-                </div>
+      <Carousel
+        autoPlay
+        infiniteLoop
+        useKeyboardArrows
+        dynamicHeight={true}
+        interval={4500}
+        transitionTime={2000}
+        showStatus={false}
+        showThumbs={false}
+      >
+        {bestsellers.map((item, idx) => (
+          <Link
+            key={idx}
+            className={styles.link}
+            to={`/product-details/${item._id}/`}
+          >
+            <div>
+              <img
+                crossOrigin="anonymous"
+                src={item.images ? item.images[0].path : null}
+                className={styles.image}
+                alt="carousel"
+              />
+              <div className={styles.legend}>
+                <h3>Najprodavaniji proizvod iz kategorije {item.category}:</h3>
+                <h3>{item.name}</h3>
               </div>
-            </Link>
-          ))}
-        </Carousel>
-      ) : (
-        <Spinner />
-      )}
+            </div>
+          </Link>
+        ))}
+      </Carousel>
     </div>
   );
 }
